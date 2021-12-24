6e23ab036652107d97be014fcba0e7a085982976
Nguyễn Công Tuân <95366547+TuanNguyen12345678@users.noreply.github.com>
2021-12-24 08:52:48 +0700

    Add files via upload

---
+++ Hiển thị danh sách quả yêu thích.py
@@ -0,0 +1,8 @@
+# Thêm quả:
+qua=["Nho","Lê","Hồng sim","Măng cụt"]
+def themqua(qua,addqua):
+  qua.append(addqua)
+  print("Thêm quả vào")
+addqua="Chôm chôm","Táo","Mận"
+themqua(qua,addqua)
+print("Quả đã được thêm vào",qua)
